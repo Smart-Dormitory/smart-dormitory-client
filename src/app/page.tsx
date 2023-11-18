ca31864400072155fde27ff925d025b97f19aa28
junoshon <sanoldfox@naver.com>
2023-11-18 22:36:29 +0900

    :lipstick: PandaCSS 테스트
    pandaCSS 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,17 @@
 'use client'
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {css} from "../../styled-system/css";
 
-const queryClient = new QueryClient({
-    defaultOptions:{
-        queries:{
-
-        }
-    }
-});
+const queryClient = new QueryClient();
 
 export default function Home() {
     return (
         <QueryClientProvider client={queryClient}>
-        
+            <div className={styles}>Hello SmartDormitory</div>
         </QueryClientProvider>
     )
 }
+
+const styles = css({
+    fontSize: '30px',
+})
